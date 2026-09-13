1177ead2ea6f67a3be81c8a7bfd278651b4ab99b
Jambbo123 <nicolai_paolo_villarin@dlsu.edu.ph>
2026-09-13 08:57:13 +0800

    Create Challenge10.cpp

---
+++ VILLARIN-FOOL-ARCANA/Challenge10.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'anagram' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+
+int anagram(string s) {
+    int n = s.length();
+    
+    if (n % 2 != 0) {
+        return -1;
+    }
+    
+    int half = n / 2;
+    int count[26] = {0};
+    
+    for (int i = 0; i < half; i++) {
+        count[s[i] - 'a']++;
+    }
+    
+    for (int i = half; i < n; i++) {
+        count[s[i] - 'a']--;
+    }
+    
+    int changes = 0;
+    
+    for (int i = 0; i < 26; i++) {
+        if (count[i] > 0) {
+            changes += count[i];
+        }
+    }
+    
+    return changes;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string q_temp;
+    getline(cin, q_temp);
+
+    int q = stoi(ltrim(rtrim(q_temp)));
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string s;
+        getline(cin, s);
+
+        int result = anagram(s);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
